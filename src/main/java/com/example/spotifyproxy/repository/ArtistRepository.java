b9ac5a78ecc8d0ef0419dd8a81e00d272edd50e0
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2022-10-27 15:17:20 +0300

    refactor: redundant import

--- src/main/java/com/example/spotifyproxy/repository/ArtistRepository.java
+++ src/main/java/com/example/spotifyproxy/repository/ArtistRepository.java
@@ -1,15 +1,13 @@
 package com.example.spotifyproxy.repository;
 
 import com.example.spotifyproxy.model.Artist;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface ArtistRepository extends JpaRepository<Artist, Long>, PagingAndSortingRepository<Artist, Long> {
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
     Optional<Artist> findArtistBySpotifyId(String spotifyId);
 
     Optional<Artist> findArtistByArtistName(String artistName);
